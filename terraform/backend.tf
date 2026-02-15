4f9020f2781f2e88390f22e1efafcd50ec97ce2f
Amarachi <amarachi32@gmail.com>
2026-02-15 13:03:29 +0100

    Update backend bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket" # Create this manually first
+    bucket         = "terraform-state-bedrock-alt-soe-025-0959"
     key            = "project-bedrock/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-locking"    # Create this manually first
